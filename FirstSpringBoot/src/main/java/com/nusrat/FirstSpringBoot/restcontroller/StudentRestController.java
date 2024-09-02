b2eed3ce6a9b0b235be1977c0c4eca276cb38090
njnusrat32856 <nusratjahannaima32856@gmail.com>
2024-09-02 18:56:29 +0600

    Update StudentRestController.java

--- FirstSpringBoot/src/main/java/com/nusrat/FirstSpringBoot/restcontroller/StudentRestController.java
+++ FirstSpringBoot/src/main/java/com/nusrat/FirstSpringBoot/restcontroller/StudentRestController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("student/api")
-public class StudentRestController {
+public class  StudentRestController {
 
     @Autowired
     private StudentService studentService;
