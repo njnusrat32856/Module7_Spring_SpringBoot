e6794488cbd49b0f3ca57a0934215c1fa4d34bef
njnusrat32856 <nusratjahannaima32856@gmail.com>
2024-09-05 11:54:23 +0600

    add

---
+++ TestSpringBoot/src/main/java/com/nusrat/TestSpringBoot/controller/HomeController.java
@@ -0,0 +1,22 @@
+package com.nusrat.TestSpringBoot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home(){
+
+
+        return "index";
+    }
+
+    @RequestMapping("/about")
+    public String about(){
+
+
+        return "about";
+    }
+}
---
+++ TestSpringBoot/src/test/java/com/nusrat/TestSpringBoot/TestSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nusrat.TestSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
