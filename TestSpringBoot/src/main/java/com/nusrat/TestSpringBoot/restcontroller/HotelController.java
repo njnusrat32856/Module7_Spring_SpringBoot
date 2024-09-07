3c3a2b24fe9565450be7d686586bcf1a580a26c7
njnusrat32856 <nusratjahannaima32856@gmail.com>
2024-09-07 15:29:05 +0600

    add

--- TestSpringBoot/src/main/java/com/nusrat/TestSpringBoot/restcontroller/HotelController.java
+++ TestSpringBoot/src/main/java/com/nusrat/TestSpringBoot/restcontroller/HotelController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/hotel")
@@ -27,5 +28,18 @@ public class HotelController {
 
         return new ResponseEntity<>("Hotel added successfully with image.", HttpStatus.OK);
     }
+
+    @GetMapping("/")
+    public ResponseEntity<List<Hotel>> getAllHotel() {
+        List<Hotel> hotels = hotelService.getAllHotel();
+        return ResponseEntity.ok(hotels);
+    }
+
+//    public ResponseEntity<List<Hotel>> findHotelByLocation(
+//            @RequestParam(value = "locationName") String locationName
+//    ) {
+//        List<Hotel> hotels = hotelService.findHotelByLocation(locationName);
+//        return ResponseEntity.ok(hotels);
+//    }
 
 }
