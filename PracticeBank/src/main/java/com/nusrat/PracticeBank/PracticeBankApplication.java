72737cf55025eec1e61b0a3b77fc6363eb3f568c
njnusrat32856 <nusratjahannaima32856@gmail.com>
2024-09-10 17:43:58 +0600

    new practice for bank

---
+++ PracticeBank/src/main/java/com/nusrat/PracticeBank/PracticeBankApplication.java
@@ -0,0 +1,13 @@
+package com.nusrat.PracticeBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeBankApplication.class, args);
+	}
+
+}
---
+++ PracticeBank/src/test/java/com/nusrat/PracticeBank/PracticeBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nusrat.PracticeBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
